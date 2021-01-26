54fadd5a1d9233848671ace129adb3deba5214f0
Mário Cardoso <mario.j.c.cardoso@hotmail.com>
2021-01-26 11:12:02 +0000

    arima multiprocessing

--- arima.py
+++ arima.py
@@ -6,30 +6,31 @@ from multiprocessing import Pool
 from functools import partial
 
 def save_prediction(prediction):
-	dataPath = 'data/medianmodel_acc_pred_arima'
+	dataPath = 'data/medianmodel_acc_pred_arima_1'
 	with open(dataPath+'.asc', 'wb') as f:
 		np.save(f, prediction, allow_pickle=False)
 
 def rmse(y_actual, y_predicted):
     return sqrt(mean_squared_error(y_actual, y_predicted) + 1e-6)
 
-def run_arima(preds, data, train_data, i):
+def run_arima(data, train_data, i):
 	if (-999.250 in train_data):
-		preds[:,i] = -999.250
-		return
+		preds = [-999.250 for i in range(122)]
+		return preds
 	else:
-		print(train_data.shape)
 		print(i)
+		print(train_data.shape)
 		in_x = np.copy(train_data)
+		preds = []
 		for j in range(122):
 			start_index = len(train_data) + j
 			end_index = start_index + 6
 			arima = sm.tsa.statespace.SARIMAX(in_x, order=(7,0,1), enforce_stationarity=False) #try with MA as (1,0,0,0,0,0,1) 
 			results = arima.fit(disp=False, maxiter=100) 
 			pred_sequence = results.predict(start=start_index, end=end_index, dynamic=False)
-			preds[j, i] = pred_sequence[-1]
+			preds.append(pred_sequence[-1])
 			in_x = np.append(in_x, data[start_index, i])
-		save_prediction(preds)
+		return preds
 
 if __name__ == '__main__':
 	dataset = AscDatasets('data', '/medianmodel_acc', 3, 1, False, 0.2, 0.2, 7, 7)
@@ -37,14 +38,19 @@ if __name__ == '__main__':
 
 	train_data = data[:-129] #165 #158
 	test_data = data[-122:]
-	preds = np.empty([122, test_data.shape[1]])
-	'''train = train_data.T
+	#preds = np.empty([122, test_data.shape[1]])
+	train = train_data.T[:int(train_data.shape[1]/2)]
 	idxs = list(range(len(train)))
-	print(idxs)
 	with Pool() as pool:
-		func = partial(run_arima, preds, data)
-		preds = pool.starmap(func, zip(train, idxs))'''
-	for i in range(test_data.shape[1]): #calculate for each time-series
+		func = partial(run_arima, data)
+		pred_matrix = pool.starmap(func, zip(train, idxs))
+		preds = np.array(pred_matrix)
+		#shape 122 x time series
+		preds = preds.T
+		print(preds.shape)
+		pool.close()
+		pool.join()
+	'''for i in range(test_data.shape[1]): #calculate for each time-series
 		if (-999.250 in train_data[:,i]):
 			preds[:,i] = -999.250
 			continue
@@ -59,12 +65,10 @@ if __name__ == '__main__':
 				pred_sequence = results.predict(start=start_index, end=end_index, dynamic=False)
 				preds[j, i] = pred_sequence[-1]
 				in_x = np.append(in_x, data[start_index, i])
-			save_prediction(preds)
-	preds = np.array(preds)
-	print(preds.shape)
-	print(test_data.shape)
-	rmse = sqrt(mean_squared_error(test_data, preds))
-	mse = mean_squared_error(test_data, preds)
+			save_prediction(preds)'''
 	preds = preds.reshape(preds.shape[0], 288, 141)
-	print('Test RMSE: %.3f' % rmse)
-	print('Test MSE: %.3f' % mse)
+	save_prediction(preds)
+	#rmse = sqrt(mean_squared_error(test_data, preds))
+	#mse = mean_squared_error(test_data, preds)
+	#print('Test RMSE: %.3f' % rmse)
+	#print('Test MSE: %.3f' % mse)
